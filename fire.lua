4fc22ca29a8db1d5e21eba2cac1695059d99df3c
Niverse-u <niversesu@gmail.com>
2023-09-20 13:16:44 +0300

    Added questions for the function

--- fire.lua
+++ fire.lua
@@ -1,5 +1,8 @@
-
-function math(x, y)
-    print(x + y)
+function math()
+    print("What is your first number?")
+    local x = io.read()
+    print("What is your second number?")
+    local y = io.read()
+    print("Here's them added: " .. x + y)
 end
-math(1, 3)
+math()
